d3ddbba797e885bba1cdc9d17339765a85b4e89c
GolubSeri <vlad-nikitienko@mail.ru>
2021-11-24 00:42:09 +0300

    Update

--- #Source/Js/script.js
+++ #Source/Js/script.js
@@ -6,3 +6,31 @@ function ibg(){
 	});
 }
 ibg();
+
+function amh(){
+	if ($(window).width() > 479){
+		$(".amh").each(function () {
+		  	var mh = 0, block_class = this.classList.item(0);
+			$("." + block_class).each(function () {
+				$("." + block_class).css('min-height', 'auto');
+				var h_block = $(this).height();
+			  	if(h_block > mh) {
+			     	mh = h_block;
+			    };
+			});
+			$("." + block_class).css('min-height', mh + 'px');
+		});
+	} else {
+		$(".amh").each(function () {
+		  	var block_class = this.classList.item(0);
+			$("." + block_class).each(function () {
+				$("." + block_class).css('min-height', 'auto');
+			});
+		});
+	}
+}
+
+amh();
+jQuery(window).resize(function() { 
+	amh();
+ });
